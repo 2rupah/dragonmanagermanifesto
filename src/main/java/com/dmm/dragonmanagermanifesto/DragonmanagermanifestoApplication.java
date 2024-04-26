ba16e2fbbf9f6d826548ce231e00e20ea675d977
2rupah <drupah824@gmail.com>
2024-04-25 22:08:48 -0400

    Initial commit

---
+++ src/main/java/com/dmm/dragonmanagermanifesto/DragonmanagermanifestoApplication.java
@@ -0,0 +1,13 @@
+package com.dmm.dragonmanagermanifesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonmanagermanifestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonmanagermanifestoApplication.class, args);
+	}
+
+}
